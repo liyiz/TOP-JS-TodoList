4538fc23f125c6196e51ecba2c6204bc463f5e10
liyiz <5160472+liyiz@users.noreply.github.com>
2025-06-16 17:34:54 -0400

    Add storage.length check to see if local storage has been used at all yet.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import './styles.css';
 import * as utilities from './utilities.js';
 
+let storage;
+
 const init = () => {
     console.log(`We have localStorage functionality: ${testfunction()}`);
+    console.dir(storage);
+    console.log(storage.length); // this let's us check if the storage has been made use of yet at all
 }
 
 const testfunction = () => {
     if (utilities.storageAvailable('localStorage')) {
+        storage = window['localStorage']; // loads the localStorage to storage variable initialized on ln 4
         return true;
     } else {
         return false;
