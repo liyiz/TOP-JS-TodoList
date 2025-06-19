8e5947f476f943b0ea4772cb265f813a0ed73ae6
liyiz <5160472+liyiz@users.noreply.github.com>
2025-06-19 14:37:29 -0400

    Add individual render functions for each todo item element

--- src/todo-item.js
+++ src/todo-item.js
@@ -2,13 +2,40 @@ const renderTodo = (container) => {
     const todoBody = document.createElement('div');
     todoBody.classList.add('page');
 
-    const title = document.createElement('h1');
-    const description = document.createElement('p');
-    const dueDate = document.createElement('p');
-    const priority = document.createElement('div');
+    const title = renderTitle('This is a title');
+    const description = renderDescription('Lorem ipsum dolor sit met. Other nonsense for a description text box so I can pad this out more with english placeholder text.');
+    const dueDate = renderDueDate('TBA');
+    const priority = renderPriority();
 
     todoBody.append(title, description, dueDate, priority);
     container.appendChild(todoBody);
+}
+
+const renderTitle = (text) => {
+    const title = document.createElement('h1');
+    title.textContent = text;
+    // add any styling or eventlistener related stuff here?
+    return title;
+}
+
+const renderDescription = (text) => {
+    const description = document.createElement('p');
+    description.textContent = text;
+    // add any styling or eventlistener related stuff here?
+    return description;
+}
+
+const renderDueDate = (date) => {
+    const dueDate = document.createElement('p');
+    dueDate.textContent = date; // This will need to be replaced with date data type
+    return dueDate;
+}
+
+const renderPriority = () => {
+    const priority = document.createElement('div');
+    // Use some kind of image for the priority?
+    priority.textContent = 'Priority placeholder';
+    return priority;
 }
 
 export {
