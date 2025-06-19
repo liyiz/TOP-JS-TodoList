12773ba7a5d2f20a70f7699bd9f8127c8e18f83b
liyiz <5160472+liyiz@users.noreply.github.com>
2025-06-19 15:47:59 -0400

    Test json save and recall from localStorage

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
 import './styles.css';
 import * as utilities from './utilities.js';
 import * as todo from './todo-item.js';
+import testjson from './example.json' assert {type: 'json'}
 
 let storage;
 
 const mainContainer = document.querySelector('main.container');
 
 const init = () => {
-    console.log(`We have localStorage functionality: ${testfunction()}`);
-    if (testfunction) {
+    console.log(`localStorage functionality: ${checkAndAssignStorage()}`);
+    if (checkAndAssignStorage) {
         console.log(storage.length); // this let's us check if the storage has been made use of yet at all
         storage.setItem("colorSetting", "#a4509b");
-        console.dir(storage);
+        // console.dir(storage);
 
         const currentColor = localStorage.getItem("colorSetting");
         const background = document.querySelector('body');
         background.style.setProperty('background-color', currentColor);
-
-        todo.renderTodo(mainContainer);
+        
+        setupStorage();
+    } else {
+        console.error('localStorage is not accessible.');
     }
+
+    todo.renderTodo(mainContainer);
+
 }
 
-const testfunction = () => {
+
+const setupStorage = () => {
+    console.log(testjson)
+    storage.setItem('userData', JSON.stringify(testjson));
+    const result = storage.getItem('userData');
+    console.log(JSON.parse(result));
+}
+
+
+const checkAndAssignStorage = () => {
     if (utilities.storageAvailable('localStorage')) {
         storage = window['localStorage']; // loads the localStorage to storage variable initialized on ln 4
         return true;
