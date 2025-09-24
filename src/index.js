0243bbbc95963fbf97a9ee9f3c49b814c16df314
liyiz <5160472+liyiz@users.noreply.github.com>
2025-09-24 14:49:20 -0400

    Implement way to select current project.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "./styles.css";
-import { Todo } from "./todo.js";
-import { Project } from "./project.js";
+import User from './user.js';
+import Todo from "./todo.js";
+import Project from "./project.js";
 
-let sessionData; // holds app data
-let user; // holds user data
+let currentUser = null;
+
 // User journey - holistic reminder //
 // 1. User should see a list of projects.
 // 2. User should be able to
@@ -29,34 +30,86 @@ const init = () => {
     if (localStorage) {
         // Get existing data
         console.log(localStorage);
-        user = JSON.parse(localStorage.getItem('userData'));
-        console.log(user);
+        const parsedData = JSON.parse(localStorage.getItem('userData'));
+        currentUser = new User(parsedData.settings, parsedData.projects);
+        
+        console.log(currentUser);
+
     } else {
         // Otherwise run first time set up -> New User class, and a default project with a default todo
     }
 
+    // Render first project into project-view
+    renderProject(0);
+
     const projectsList = document.getElementById('projects-list');
     projectsList.innerHTML = '';
     
-    user.projects.forEach((project) => {
-        const newItem = document.createElement('li');
+    currentUser.projects.forEach((project, index) => {
+        const li = document.createElement('li');
+        li.setAttribute('index', index);
 
-        const itemSpan = document.createElement('span');
-        itemSpan.textContent = project.title;
-        // maybe consider adding data attributes at this point?
+        li.addEventListener('click', function () {
+            // currentProjectIndex = index;
+            // console.log(index);
+            // call function that renders project view to show project todos
+            renderProject(index);
+        });
+
+        const span = document.createElement('span');
+        span.textContent = project.title;
 
         const btnDiv = document.createElement('div');
         const btnEdit = document.createElement('button');
         const btnDelete = document.createElement('button');
+        // TODO May need to set other button form attributes
+
+        btnEdit.addEventListener('click', (event) => {
+            event.stopPropagation();
+            console.log(`Edit project index ${index}`);
+            // project.edit(index);
+        });
+        btnDelete.addEventListener('click', (event) => {
+            event.stopPropagation();
+            console.log(`Delete project index ${index}`);
+            // project.delete(index);
+        });
+
         btnEdit.textContent = 'edit';
         btnDelete.textContent = 'delete';
         btnDiv.append(btnEdit, btnDelete);
 
-        newItem.append(itemSpan, btnDiv);
+        li.append(span, btnDiv);
 
-        projectsList.append(newItem);
+        projectsList.append(li);
     });
 
 };
+
+function renderProject(projectIndex) {
+
+    const main = document.getElementById('project-view');
+    const title = document.getElementById('project-title');
+    title.textContent = currentUser.projects[projectIndex].title;
+
+    const todos = document.getElementById('todos-list');
+    todos.innerHTML = '';
+
+    if (currentUser.projects[projectIndex].todos.length <= 0) {
+        return console.error('This project has an empty todos array!');
+    }
+
+    currentUser.projects[projectIndex].todos.forEach((todo) => {
+        const li = document.createElement('li');
+        const checkbox = document.createElement('input');
+        checkbox.id = ''; // TODO
+        checkbox.type = 'checkbox';
+        checkbox.name = ''; // TODO
+        checkbox.checked = todo.completed; // TODO
+        const span = document.createElement('span');
+        span.textContent = todo.title;
+    });
+    
+}
 
 window.addEventListener("DOMContentLoaded", init);
